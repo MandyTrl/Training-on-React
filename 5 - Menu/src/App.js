55f0792e335e268d436c24e0d90eef939563fa20
MandyTrl <mandythorel@gmail.com>
2022-08-24 12:46:03 +0200

    Project 5 : Ok

---
+++ 5 - Menu/src/App.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
+
+function App() {
+  const [menuItems, SetMenuItems] = useState(items); ///Variable d'état qui va setter les données du fichier "data.js"
+  const [categories, SetCategories] = useState([]); ///Variable d'état qui va setter le composant "Catégories" initialisée à un tableau vide
+  
+  const filterItems = (category) => {
+    if(category === "all"){
+      SetMenuItems(items);
+      return;
+    }
+    const newItems = items.filter( (item) =>
+    item.category === category)
+    SetMenuItems(newItems);
+  } ///Filtre les plats en fonction de la catégorie sélectionnée
+
+  return <main>
+    <section className="menu section">
+      <div className="title">
+        <h2>Our Menu</h2>
+        <div className="underline"/>
+        <Categories filterItems={filterItems}/>
+        <Menu items={menuItems}/>
+      </div>
+    </section>
+  </main>;
+}
+
+export default App;

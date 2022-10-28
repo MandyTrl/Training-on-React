97dc7714f60396bdf9300ec33b7718e8e08c81ba
MandyTrl <mandythorel@gmail.com>
2022-10-28 15:06:14 +0200

    empty list + remove item

--- 10 - Grocery Bud/src/App.js
+++ 10 - Grocery Bud/src/App.js
@@ -10,32 +10,42 @@ function App() {
   const [editID, setEditID] = useState(null);
   const [alert, setAlert] = useState({
     show: true, 
-    msg: "hello world", 
-    type: "success"});
+    msg: "", 
+    type: ""});
   const handleSubmit = (e)=>{
     e.preventDefault();
     if(!name){
       //display alert
-      setAlert({
-        show : true,
-        msg: "please enter value",
-        type: "danger",
-    })
+      showAlert(true, "danger", "Please enter value")
     } else if (name && isEditing) {
       //deal with edit
     } else {
       //show alert
+      showAlert(true, "success", "item added to the list")
       const newItem = {id : new Date().getTime().toString(), title : name};
       setList([...list, newItem]);
       setName("");
     }
-    console.log("Hello")
   }
 
+  const showAlert = ( show= false, type= "", msg= "" ) => {
+    setAlert({show: show, type, msg})
+  }
+
+  const clearList = () => {
+    showAlert(true, "danger", "empty list")
+    setList([])
+  }
+
+  const removeItem = (id) => {
+    showAlert(true, "danger", "item removed")
+    setList([list.filter( ( item ) => item.id !== id )])
+  } 
+
   return (
-  <section className= "section-center">
+    <section className= "section-center">
       <form className= "grocery-form" onSubmit= {handleSubmit}>
-        {alert.show && <Alert {...alert} />}
+        {alert.show && <Alert {...alert} removeAlert= {showAlert} />}
         
         <h3>Grocery Bud</h3>
         <div className= "form-control">
@@ -51,8 +61,8 @@ function App() {
       </form>
       {list.length > 0 && (
         <div className= "grocery-container">
-        <List items= {list} />
-          <button className= "clear-btn">
+        <List items= {list} removeItem={removeItem} />
+          <button className= "clear-btn" onClick={clearList}>
             clear items
           </button>
         </div>
